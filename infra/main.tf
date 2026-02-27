259636ec7067cda285a384dcfb78b0f9b7d34188
Meetkumar Patel <pmeet464@gmail.com>
2026-02-27 18:53:27 +0530

    Add Azure AD authentication configuration to Static Web App
    - Added azuread provider and Azure AD app registration
    - Created client roles dynamically from var.clients
    - Configured Static Web App authentication with Azure AD
    - Added authentication outputs for configuration
    - Enables proper role-based access control for client routes

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.100"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.50"
+    }
   }
 
   backend "azurerm" {}
@@ -14,6 +18,8 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azuread" {}
 
 # -----------------------------
 # Resource Group
@@ -42,6 +48,47 @@ resource "azurerm_static_web_app" "swa" {
 }
 
 # -----------------------------
+# Azure AD App Registration
+# -----------------------------
+
+resource "azuread_application" "swa_app" {
+  display_name = "${var.static_web_app_name}-auth"
+
+  dynamic "app_role" {
+    for_each = var.clients
+    content {
+      allowed_member_types = ["User"]
+      description          = "Access to ${app_role.key}"
+      display_name         = app_role.key
+      enabled              = true
+      id                   = uuidv5("dns", app_role.key)
+      value                = app_role.key
+    }
+  }
+}
+
+resource "azuread_service_principal" "swa_sp" {
+  client_id = azuread_application.swa_app.client_id
+}
+
+# -----------------------------
+# Static Web App Authentication
+# -----------------------------
+
+resource "azurerm_static_web_app_authentication" "auth" {
+  static_web_app_id = azurerm_static_web_app.swa.id
+  
+  identity_provider {
+    provider_type = "AzureActiveDirectory"
+    registration {
+      client_id = azuread_application.swa_app.client_id
+      client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
+      issuer_authority = "https://sts.windows.net/${azuread_application.swa_app.sign_in_audience}/"
+    }
+  }
+}
+
+# -----------------------------
 # Outputs
 # -----------------------------
 
@@ -51,6 +98,14 @@ output "static_web_app_hostname" {
 
 output "static_web_app_identity_principal_id" {
   value = azurerm_static_web_app.swa.identity[0].principal_id
+}
+
+output "app_registration_client_id" {
+  value = azuread_application.swa_app.client_id
+}
+
+output "app_registration_object_id" {
+  value = azuread_application.swa_app.object_id
 }
 
 output "static_web_app_identity_tenant_id" {
