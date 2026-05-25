16f81275d0c5561ac96f461d2d05c2eafd416728
Meetkumar Patel <pmeet464@gmail.com>
2026-05-25 18:19:54 +0530

    chore(terraform): comment out unused Azure AD resources for SWA built-in auth
    - disable azuread provider configuration
    - comment out app registration and service principal resources
    - retain code for future enterprise Entra integration
    - simplify infrastructure for contributor-level access
    - continue using Azure Static Web Apps built-in authentication

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,13 @@ terraform {
       version = "~> 3.117"
     }
 
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 3.0"
-    }
+    # Not required for current SWA built-in auth implementation
+    # Can be enabled later if Entra ID admin access is available
+    #
+    # azuread = {
+    #   source  = "hashicorp/azuread"
+    #   version = "~> 3.0"
+    # }
   }
 
   backend "azurerm" {}
@@ -20,7 +23,8 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuread" {}
+# Not required for Azure Static Web Apps built-in authentication
+# provider "azuread" {}
 
 # -----------------------------
 # Resource Group
@@ -51,26 +55,34 @@ resource "azurerm_static_web_app" "swa" {
 # -----------------------------
 # Azure AD App Registration
 # -----------------------------
-
-resource "azuread_application" "swa_app" {
-  display_name = "${var.static_web_app_name}-auth"
-
-  dynamic "app_role" {
-    for_each = var.clients
-    content {
-      allowed_member_types = ["User"]
-      description          = "Access to ${app_role.key}"
-      display_name         = app_role.key
-      enabled              = true
-      id                   = uuidv5("dns", app_role.key)
-      value                = app_role.key
-    }
-  }
-}
-
-resource "azuread_service_principal" "swa_sp" {
-  client_id = azuread_application.swa_app.client_id
-}
+# Not required for current implementation.
+# Azure Static Web Apps built-in authentication is being used:
+#   /.auth/login/aad
+#
+# Can be enabled later if:
+# - custom Entra ID integration is needed
+# - custom claims/groups are required
+# - enterprise app registration access is available
+#
+# resource "azuread_application" "swa_app" {
+#   display_name = "${var.static_web_app_name}-auth"
+#
+#   dynamic "app_role" {
+#     for_each = var.clients
+#     content {
+#       allowed_member_types = ["User"]
+#       description          = "Access to ${app_role.key}"
+#       display_name         = app_role.key
+#       enabled              = true
+#       id                   = uuidv5("dns", app_role.key)
+#       value                = app_role.key
+#     }
+#   }
+# }
+#
+# resource "azuread_service_principal" "swa_sp" {
+#   client_id = azuread_application.swa_app.client_id
+# }
 
 # -----------------------------
 # Outputs
@@ -84,13 +96,15 @@ output "static_web_app_identity_principal_id" {
   value = azurerm_static_web_app.swa.identity[0].principal_id
 }
 
-output "app_registration_client_id" {
-  value = azuread_application.swa_app.client_id
-}
-
-output "app_registration_object_id" {
-  value = azuread_application.swa_app.object_id
-}
+# Not required for current implementation
+#
+# output "app_registration_client_id" {
+#   value = azuread_application.swa_app.client_id
+# }
+#
+# output "app_registration_object_id" {
+#   value = azuread_application.swa_app.object_id
+# }
 
 output "static_web_app_identity_tenant_id" {
   value = azurerm_static_web_app.swa.identity[0].tenant_id
